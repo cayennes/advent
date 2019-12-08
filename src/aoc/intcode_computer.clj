bdf1ae0593419c9612130862aaf746906214f6f7
Cayenne Geis <cayennes@gmail.com>
2019-12-08 11:00:48 -0500

    Factor intcode-computer tests into actual test file

--- src/aoc/intcode_computer.clj
+++ src/aoc/intcode_computer.clj
@@ -1,8 +1,7 @@
 (ns aoc.intcode-computer
   (:require [clojure.edn :as edn]
             [clojure.java.io :as io]
-            [clojure.string :as string]
-            [clojure.test :refer [deftest is run-tests]]))
+            [clojure.string :as string]))
 
 (defn parse
   [input]
@@ -71,26 +70,11 @@
         (update :input rest)
         (update :position + 2))))
 
-(deftest op-3-works
-  (is (= [16 0 99]
-         (:program (exec-once (new-computer [3 0 99] [16])))))
-  (is (= {:program [3 0 99]
-          :awaiting-input true}
-         (select-keys (exec-once (new-computer [3 0 99] []))
-                      [:program :awaiting-input])))
-  (is (= {:program [0 0 99]}
-         (select-keys (exec-once (assoc (new-computer [3 0 99] [0])
-                                        :awaiting-input true))
-                      [:program :awaiting-input]))))
-
 (defmethod exec-once 4
   [{:keys [program position] :as computer}]
   (-> computer
       (update :output conj (read-parameter program position 1 false))
       (update :position + 2)))
-
-(deftest op-4-works
-  (is (= [4] (:output (exec-once {:program [4 0 99] :position 0})))))
 
 (defmethod exec-once 5
   [{:keys [program position] :as computer}]
@@ -122,18 +106,13 @@
        (empty? (:input computer))))
 
 (defn exec-all
-  {:test #(is (= 17 (-> (new-computer [3 0 4 0 99] [17])
-                        (exec-all)
-                        :output
-                        last)))}
   [computer]
   (->> (iterate exec-once computer)
        (drop-while #(not (or (:halt %)
                              (needs-input? %))))
        first))
 
-(defn day2a
-  {:test #(is (= 4576384 (day2a)))}
+(defn day2-1
   []
   (-> (parse (slurp (io/resource "day2")))
       (assoc 1 12
@@ -143,7 +122,7 @@
       (:program)
       (first)))
 
-(defn day2b
+(defn day2-2
   []
   (let [input (parse (slurp (io/resource "day2")))]
     (first (for [noun (range 100)
@@ -156,11 +135,7 @@
                 :when (= 19690720 result)]
             (+ verb (* 100 noun))))))
 
-(comment "slow test"
-  (= 5398 (day2b)))
-
 (defn day5-1
-  {:test #(is (= 15314507 (day5-1)))}
   []
   (-> (parse (slurp (io/resource "day5")))
       (new-computer [1])
@@ -168,28 +143,7 @@
       (:output)
       (last)))
 
-(deftest day5-2-examples
-  (is (= 1 
-         (-> (new-computer [3 9 8 9 10 9 4 9 99 -1 8]
-                           [8])
-             (exec-all)
-             (:output)
-             (last))))
-  (is (= 0 
-         (-> (new-computer [3 3 1108 -1 8 3 4 3 99]
-                           [7])
-             (exec-all)
-             (:output)
-             (last))))
-  (is (= 1 
-         (-> (new-computer [3 12 6 12 15 1 13 14 13 4 13 99 -1 0 1 9]
-                           [8])
-             (exec-all)
-             (:output)
-             (last)))))
-
 (defn day5-2
-  {:test #(is (= 652726 (day5-2)))}
   []
   (-> (parse (slurp (io/resource "day5")))
       (new-computer [5])
@@ -208,29 +162,10 @@
   (mapv #(new-computer program [%]) phases))
 
 (defn add-first-input
-  {:test #(is [{:input [1 2]}] (add-first-input [{:input [1]}] 2))}
   [computers input]
   (update-in computers [0 :input] #(concat % [input])))
 
 (defn run-multiple
-  {:test #(do (is (= 43210
-                     (-> (amplifier-vec [3 15 3 16 1002 16 10 16 1 16 15 15 4 15 99 0 0]
-                                        [4 3 2 1 0])
-                         (add-first-input 0)
-                         (run-multiple)
-                         (final-output))))
-              (is (= 54321
-                     (-> (amplifier-vec [3 23 3 24 1002 24 10 24 1002 23 -1 23 101 5 23 23 1 24 23 23 4 23 99 0 0]
-                                        [0 1 2 3 4])
-                         (add-first-input 0)
-                         (run-multiple)
-                         (final-output))))
-              (is (= 65210
-                     (-> (amplifier-vec [3 31 3 32 1002 32 10 32 1001 31 -2 31 1007 31 0 33 1002 33 7 33 1 33 31 31 1 32 31 31 4 31 99 0 0 0]
-                                        [1 0 4 3 2])
-                         (add-first-input 0)
-                         (run-multiple)
-                         (final-output)))))}
   [computers]
   (vec
    (reductions (fn [previous-computer next-computer]
@@ -250,34 +185,10 @@
              (map #(conj % item) (permutations others))))))
 
 (defn loop-input
-  {:test #(is (= [{:input [78]} {:output [56 78]}]
-                 (loop-input [{} {:output [56 78]}])))}
   [computers]
   (add-first-input computers (last (:output (last computers)))))
 
 (defn run-multiple-until-done
-  {:test #(do
-            (is (= 43210
-                   (final-output
-                    (run-multiple-until-done
-                     (add-first-input
-                      (amplifier-vec [3 15 3 16 1002 16 10 16 1 16 15 15 4 15 99 0 0]
-                                     [4 3 2 1 0])
-                      0)))))
-            (is (= 139629729
-                     (final-output
-                      (run-multiple-until-done
-                       (add-first-input
-                        (amplifier-vec [3 26 1001 26 -4 26 3 27 1002 27 2 27 1 27 26 27 4 27 1001 28 -1 28 1005 28 6 99 0 0 5]
-                                       [9 8 7 6 5])
-                        0)))))
-            (is (= 18216
-                   (final-output
-                    (run-multiple-until-done
-                     (add-first-input
-                      (amplifier-vec [3 52 1001 52 -5 52 3 53 1 52 56 54 1007 54 5 55 1005 55 26 1001 54 -5 54 1105 1 12 1 53 54 53 1008 54 0 55 1001 55 1 55 2 53 55 53 4 53 1001 56 -1 56 1005 56 6 99 0 0 0 0 10]
-                                     [9 7 8 5 6])
-                      0))))))}
   [computers]
   (let [first-run (run-multiple computers)]
     (->> first-run
@@ -287,8 +198,6 @@
          (first))))
 
 (defn find-max-amplification
-  {:test #(is (= 43210
-                 (find-max-amplification [3 15 3 16 1002 16 10 16 1 16 15 15 4 15 99 0 0] (range 5))))}
   [program phases]
   (apply max
          (for [phase-order (permutations phases)]
@@ -298,15 +207,11 @@
                (final-output)))))
 
 (defn day7-1
-  {:test #(is (= 272368 (day7-1)))}
   []
   (-> (io/resource "day7") (slurp) (parse)
       (find-max-amplification (range 5))))
 
 (defn day7-2
-  {:test #(is (= 19741286 (day7-2)))}
   []
   (-> (io/resource "day7") (slurp) (parse)
       (find-max-amplification (range 5 10))))
-
-(run-tests)
---
+++ test/aoc/intcode_computer_test.clj
@@ -0,0 +1,116 @@
+(ns aoc.intcode-computer-test
+  (:require [clojure.test :refer [deftest is]]
+            [aoc.intcode-computer :as ic]))
+
+(deftest op-3-works
+  (is (= [16 0 99]
+         (:program (ic/exec-once (ic/new-computer [3 0 99] [16])))))
+  (is (= {:program [3 0 99]
+          :awaiting-input true}
+         (select-keys (ic/exec-once (ic/new-computer [3 0 99] []))
+                      [:program :awaiting-input])))
+  (is (= {:program [0 0 99]}
+         (select-keys (ic/exec-once (assoc (ic/new-computer [3 0 99] [0])
+                                           :awaiting-input true))
+                      [:program :awaiting-input]))))
+
+(deftest op-4-works
+  (is (= [4] (:output (ic/exec-once {:program [4 0 99] :position 0})))))
+
+(deftest exec-all-works
+  (is (= 17 (-> (ic/new-computer [3 0 4 0 99] [17])
+                (ic/exec-all)
+                :output
+                last))))
+
+(deftest day2-1-result
+  (is (= 4576384 (ic/day2-1))))
+
+(comment "day2-2 result is slower than I want to test"
+  (= 5398 (ic/day2-2)))
+
+(deftest day5-1-result
+  (is (= 15314507 (ic/day5-1))))
+
+(deftest day5-2-examples
+  (is (= 1 
+         (-> (ic/new-computer [3 9 8 9 10 9 4 9 99 -1 8]
+                              [8])
+             (ic/exec-all)
+             (:output)
+             (last))))
+  (is (= 0 
+         (-> (ic/new-computer [3 3 1108 -1 8 3 4 3 99]
+                              [7])
+             (ic/exec-all)
+             (:output)
+             (last))))
+  (is (= 1 
+         (-> (ic/new-computer [3 12 6 12 15 1 13 14 13 4 13 99 -1 0 1 9]
+                              [8])
+             (ic/exec-all)
+             (:output)
+             (last)))))
+
+(deftest day5-2-result
+  (is (= 652726 (ic/day5-2))))
+
+(deftest add-first-input-works
+  (is [{:input [1 2]}] (ic/add-first-input [{:input [1]}] 2)))
+
+(deftest run-multiple-works
+  (is (= 43210
+         (-> (ic/amplifier-vec [3 15 3 16 1002 16 10 16 1 16 15 15 4 15 99 0 0]
+                            [4 3 2 1 0])
+             (ic/add-first-input 0)
+             (ic/run-multiple)
+             (ic/final-output))))
+  (is (= 54321
+         (-> (ic/amplifier-vec [3 23 3 24 1002 24 10 24 1002 23 -1 23 101 5 23 23 1 24 23 23 4 23 99 0 0]
+                            [0 1 2 3 4])
+             (ic/add-first-input 0)
+             (ic/run-multiple)
+             (ic/final-output))))
+  (is (= 65210
+         (-> (ic/amplifier-vec [3 31 3 32 1002 32 10 32 1001 31 -2 31 1007 31 0 33 1002 33 7 33 1 33 31 31 1 32 31 31 4 31 99 0 0 0]
+                            [1 0 4 3 2])
+             (ic/add-first-input 0)
+             (ic/run-multiple)
+             (ic/final-output)))))
+
+(deftest loop-input-works
+  (is (= [{:input [78]} {:output [56 78]}]
+         (ic/loop-input [{} {:output [56 78]}]))))
+
+(deftest run-multiple-until-done-works
+  (is (= 43210
+         (ic/final-output
+          (ic/run-multiple-until-done
+           (ic/add-first-input
+            (ic/amplifier-vec [3 15 3 16 1002 16 10 16 1 16 15 15 4 15 99 0 0]
+                              [4 3 2 1 0])
+            0)))))
+  (is (= 139629729
+         (ic/final-output
+          (ic/run-multiple-until-done
+           (ic/add-first-input
+            (ic/amplifier-vec [3 26 1001 26 -4 26 3 27 1002 27 2 27 1 27 26 27 4 27 1001 28 -1 28 1005 28 6 99 0 0 5]
+                              [9 8 7 6 5])
+            0)))))
+  (is (= 18216
+         (ic/final-output
+          (ic/run-multiple-until-done
+           (ic/add-first-input
+            (ic/amplifier-vec [3 52 1001 52 -5 52 3 53 1 52 56 54 1007 54 5 55 1005 55 26 1001 54 -5 54 1105 1 12 1 53 54 53 1008 54 0 55 1001 55 1 55 2 53 55 53 4 53 1001 56 -1 56 1005 56 6 99 0 0 0 0 10]
+                              [9 7 8 5 6])
+            0))))))
+
+(deftest find-max-amplification-works
+  (is (= 43210
+          (ic/find-max-amplification [3 15 3 16 1002 16 10 16 1 16 15 15 4 15 99 0 0] (range 5)))))
+
+(deftest day7-1-result
+  (is (= 272368 (ic/day7-1))))
+
+(deftest day7-2-result
+  (is (= 19741286 (ic/day7-2))))
