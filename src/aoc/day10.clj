a5802c430495a6cc63d8db5e4e4ddc724cd94cd3
Cayenne Geis <cayennes@gmail.com>
2019-12-10 11:51:39 -0500

    day 10 part 1

--- src/aoc/day10.clj
+++ src/aoc/day10.clj
@@ -3,10 +3,10 @@
             [clojure.string :as string]
             [clojure.test :refer [is deftest]]))
 
-(defn list-locations
-  {:test #(do (is (= #{[0 0] [1 1] [1 0]} (set (list-locations "##\n.#"))))
+(defn list-asteroids
+  {:test #(do (is (= #{[0 0] [1 1] [1 0]} (set (list-asteroids "##\n.#"))))
               (is (= #{[1 0] [4 0] [0 2] [1 2] [2 2] [3 2] [4 2] [4 3] [3 4] [4 4]}
-                     (set (list-locations ".#..#\n.....\n#####\n....#\n...##")))))}
+                     (set (list-asteroids ".#..#\n.....\n#####\n....#\n...##")))))}
   [diagram-string]
   (filter some?
           (apply concat
@@ -19,54 +19,56 @@
                      row))
                   (string/split diagram-string #"\n")))))
 
-(defn count-asteroid-angles
-  [asteroids [base-x base-y]]
+(defn angle
+  [[base-x base-y] [asteroid-x asteroid-y]]
+  (cond
+    (= asteroid-y base-y)
+    (if (< base-x asteroid-x)
+      :pos-flat
+      :neg-flat)
+
+    (= asteroid-x base-x)
+    (if (< base-y asteroid-y)
+      :pos-vert
+      :neg-vert)
+
+    :else [(/ (- asteroid-x base-x) (- asteroid-y base-y))
+           (< asteroid-x base-x)]))
+
+(defn asteroid-direction
+  [asteroids base]
   (->> asteroids
-       (filter #(not= [base-x base-y] %))
-       (map (fn [[asteroid-x asteroid-y]]
-              (cond
-                (zero? (- asteroid-y base-y))
-                (if (pos? (- asteroid-x base-x))
-                  :pos-flat
-                  :neg-flat)
-
-                (zero? (- asteroid-x base-x))
-                (if (pos? (- asteroid-y base-y))
-                  :pos-vert
-                  :neg-vert)
-
-                :else (/ (- asteroid-x base-x)
-                         (- asteroid-y base-y)))))
-       (distinct)
-       (count)))
+       (filter #(not= base %))
+       (group-by #(angle base %))))
 
 (defn find-best-location
   {:test #(is (= 8 (find-best-location [[1 0] [4 0] [0 2] [1 2] [2 2] [3 2] [4 2] [4 3] [3 4] [4 4]])))}
   [asteroids]
-  (apply max (map #(count-asteroid-angles asteroids %)
+  (apply max (map #(count (asteroid-direction asteroids %))
                   asteroids)))
 
 (deftest day10-1-examples
   (is (= 33
          (-> "......#.#.\n#..#.#....\n..#######.\n.#.#.###..\n.#..#.....\n..#....#.#\n#..#....#.\n.##.#..###\n##...#..#.\n.#....####"
-             list-locations
+             list-asteroids
              find-best-location)))
 
   (is (= 35
          (-> "#.#...#.#.\n.###....#.\n.#....#...\n##.#.#.#.#\n....#.#.#.\n.##..###.#\n..#...##..\n..##....##\n......#...\n.####.###."
-             list-locations
+             list-asteroids
              find-best-location)))
   (is (= 41
          (-> ".#..#..###\n####.###.#\n....###.#.\n..###.##.#\n##.##.#.#.\n....###..#\n..#.#..#.#\n#..#.#.###\n.##...##.#\n.....#.#.."
-             list-locations
+             list-asteroids
              find-best-location)))
   (is (= 210
-         (->".#..##.###...#######\n##.############..##.\n.#.######.########.#\n.###.#######.####.#.\n#####.##.#.##.###.##\n..#####..#.#########\n####################\n#.####....###.#.#.##\n##.#################\n#####.##.###..####..\n..######..##.#######\n####.##.####...##..#\n.#####..#.######.###\n##...#.##########...\n#.##########.#######\n.####.#.###.###.#.##\n....##.##.###..#####\n.#.#.###########.###\n#.#.#.#####.####.###\n###.##.####.##.#..##"
-            list-locations
+         (-> ".#..##.###...#######\n##.############..##.\n.#.######.########.#\n.###.#######.####.#.\n#####.##.#.##.###.##\n..#####..#.#########\n####################\n#.####....###.#.#.##\n##.#################\n#####.##.###..####..\n..######..##.#######\n####.##.####...##..#\n.#####..#.######.###\n##...#.##########...\n#.##########.#######\n.####.#.###.###.#.##\n....##.##.###..#####\n.#.#.###########.###\n#.#.#.#####.####.###\n###.##.####.##.#..##"
+            list-asteroids
             find-best-location))))
 
 (defn day10-1
+  {:test #(is (= 309 (day10-1)))}
   []
   (-> (io/resource "day10") (slurp)
-      (list-locations)
+      (list-asteroids)
       (find-best-location)))
