d183d62d74c1e82e5c3072dd6b9b510094245508
Cayenne Geis <cayennes@gmail.com>
2019-12-14 18:03:34 -0500

    day 14 part 1

--- src/aoc/day14.clj
+++ src/aoc/day14.clj
@@ -5,40 +5,61 @@
 
 (defn parse-line
   [reaction-string]
-  (let [[product & reagents] (->> reaction-string
-                                  (re-seq #"(\d+) ([A-Z]+)")
-                                  (map rest)
-                                  (reverse)
-                                  (map #(mapv edn/read-string %))
-                                  (map #(update % 1 keyword)))]
-    [(first product) {:yield (second product)
-                      :reagents (into {} reagents)}]))
+  (let [[product & ingredients] (->> reaction-string
+                                     (re-seq #"(\d+) ([A-Z]+)")
+                                     (map rest)
+                                     (reverse)
+                                     (map #(mapv edn/read-string %))
+                                     (map #(update % 1 keyword)))]
+    [(second product) {:yield (first product)
+                       :ingredients (zipmap (map second ingredients)
+                                            (map first ingredients))}]))
 
 (defn parse
   "parses reaction input to look like
 
-  {:AAA {:yield 1 :reagents {:BBB 2 :CCC 1}} ...} "
+  {:AAA {:yield 1 :ingredients {:BBB 2 :CCC 1}} ...} "
   [input]
-  (-> input
-      (string/split #"\n")
-      (->> (map parse-line))
-      (into {})))
+  (->> (string/split input #"\n")
+       (map parse-line)
+       (into {})))
 
-(defn done?
-  [{:keys [needed]}]
-  (= [:ORE] (keys needed)))
+(defn decrease-in
+  [m k n]
+  (cond
+    (= n (m k)) (dissoc m k)
+    (< n (m k)) (update m k - n)))
 
-(defn plan-step
-  "replace something in needed with its ingredients, keeping track of spares
+;; input and output of these functions looks like
+;; {:needed {:AAA 6 :BBB 2 ...} :spares {:CCC 2 ...}}
 
-  input and output looks like
-  {:needed {:AAA 6 :BBB 2 ...} :inventory {:CCC 2 ...}} "
-  [{:keys [needed inventory]} reactions]
-  (let [chemical (first (filter #(not= :ORE %) (keys needed)))
-        {:keys [yield reagents]} (reaction chemical)]
+(defn take-from-spares
+  [{:keys [needed spares] :as plan} chemical]
+  (if-let [available (spares chemical)]
+    (let [amount (min (needed chemical) available)]
+      {:spares (decrease-in spares chemical amount)
+       :needed (decrease-in needed chemical amount)})
+    plan))
 
-    ))
+(defn create-with-reaction
+  [{:keys [needed spares] :as plan} chemical reaction]
+  (if (needed chemical)
+    {:needed (merge-with + (:ingredients reaction) needed)
+     :spares (merge-with + spares {chemical (:yield reaction)})}
+    plan))
+
+(defn plan-all
+  [reactions]
+  (loop [plan {:needed {:FUEL 1} :spares {}}]
+    (if (= [:ORE] (keys (:needed plan)))
+      (get-in plan [:needed :ORE])
+      (let [chemical (->> plan :needed keys (filter #(not= % :ORE)) first)]
+         (-> plan
+             (take-from-spares chemical)
+             (create-with-reaction chemical (reactions chemical))
+             (recur))))))
 
 (defn part1
   []
-  (util/read-input "day14" parse))
+  (plan-all (util/read-input "day14" parse)))
+
--- test/aoc/day14_test.clj
+++ test/aoc/day14_test.clj
@@ -4,4 +4,29 @@
 
 
 (deftest parse-line-works
-  (is (= [:A {:yield 10 :reagents {:ORE 10}}] (d/parse-line "10 ORE => 10 A"))))
+  (is (= [:A {:yield 10 :ingredients {:ORE 10}}]
+         (d/parse-line "10 ORE => 10 A"))))
+
+(deftest parse-works
+  (is (= {:A {:yield 10 :ingredients {:ORE 10}}
+          :B {:yield 1 :ingredients {:ORE 1}}
+          :C {:yield 1 :ingredients {:B 1 :A 7}}
+          :D {:yield 1 :ingredients {:C 1 :A 7}}
+          :E {:yield 1 :ingredients {:D 1 :A 7}}
+          :FUEL {:yield 1 :ingredients {:E 1 :A 7}}}
+         (d/parse "10 ORE => 10 A\n1 ORE => 1 B\n7 A, 1 B => 1 C\n7 A, 1 C => 1 D\n7 A, 1 D => 1 E\n7 A, 1 E => 1 FUEL"))))
+
+(deftest decrease-in-works
+  (is (= {:A 3 :B 2} (d/decrease-in {:A 7 :B 2} :A 4)))
+  (is (= {:B 2} (d/decrease-in {:A 4 :B 2} :A 4))))
+
+(deftest take-from-spares-works
+  (is (= {:needed {:A 3} :spares {}}
+         (d/take-from-spares {:needed {:A 5} :spares {:A 2}} :A))))
+
+(deftest create-with-reaction-works
+  (is (= {:needed {:A 2 :B 6} :spares {:A 3}}
+         (d/create-with-reaction {:needed {:A 2} :spares {}}
+                                 [:A {:yield 3 :ingredients {:B 6}}]))))
+(deftest plan-all-works
+  (is (= 10 (d/plan-all {:FUEL {:yield 1 :ingredients {:ORE 10}}}))))
