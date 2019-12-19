a71eacad1b1f3f78a3ee07e3e9918e60ca646805
Cayenne Geis <cayenne@circleci.com>
2019-12-19 08:36:01 -0500

    day 19 display function and memoize drone

--- src/aoc/day19.clj
+++ src/aoc/day19.clj
@@ -4,7 +4,7 @@
 
 (def coords (for [x (range 50) y (range 50)] [x y]))
 
-(defn get-influence
+(defn get-influence*
   [coord]
   (-> (util/read-input "day19" ic/parse)
       (ic/new-computer)
@@ -13,6 +13,18 @@
       (:output)
       (first)))
 
+(def get-influence (memoize get-influence*))
+
 (defn part1
   []
   (apply + (for [x (range 50) y (range 50)] (get-influence [x y]))))
+
+(defn display
+  [x1 x2 y1 y2]
+  (doseq [y (range y1 (inc y2))]
+    (println
+     (apply str
+            (for [x (range x1 (inc x2))]
+              (if (= 1 (get-influence [x y]))
+                "#"
+                "."))))))
