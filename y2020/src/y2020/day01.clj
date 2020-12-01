eb7010098fb95346e6daef9caff7576302d7c59d
Cayenne Geis <cayenne@hey.com>
2020-12-01 07:14:44 -0500

    2020 day 1

--- y2020/src/y2020/day01.clj
+++ y2020/src/y2020/day01.clj
@@ -1,2 +1,28 @@
 (ns y2020.day01
-  (:require [y2020.util :as util]))
+  (:require [clojure.edn :as edn]
+            [clojure.string :as string]
+            [y2020.util :as util]))
+
+(defn parse
+  [s]
+  (for [line (string/split s #"\n")]
+    (edn/read-string line)))
+
+(defn part1
+  []
+  (let [input (util/read-input "day01.txt" parse)]
+    (first
+     (for [x input
+           y input
+           :when (= 2020 (+ x y))]
+       (* x y)))))
+
+(defn part2
+  []
+  (let [input (util/read-input "day01.txt" parse)]
+    (first
+     (for [x input
+           y input
+           z input
+           :when (= 2020 (+ x y z))]
+       (* x y z)))))
