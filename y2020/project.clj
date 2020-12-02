d8a747d206830de000c0c9e0c35e891acdc2002b
Cayenne Geis <cayenne@hey.com>
2020-12-01 19:19:29 -0500

    use indices
    * works in the edge case where a repeated number could make 2020
    * makes part2 faster

--- y2020/project.clj
+++ y2020/project.clj
@@ -1,4 +1,5 @@
 (defproject y2020 "0.1.0-SNAPSHOT"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]])
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [criterium "0.4.6"]])
--- y2020/src/y2020/day01.clj
+++ y2020/src/y2020/day01.clj
@@ -1,28 +1,37 @@
 (ns y2020.day01
   (:require [clojure.edn :as edn]
             [clojure.string :as string]
+            [criterium.core :as criterium]
             [y2020.util :as util]))
 
 (defn parse
   [s]
-  (for [line (string/split s #"\n")]
-    (edn/read-string line)))
+  (vec (for [line (string/split s #"\n")] ;
+         (edn/read-string line))))
 
 (defn part1
   []
-  (let [input (util/read-input "day01.txt" parse)]
+  (let [input (util/read-input "day01.txt" parse)
+        input-count (count input)]
     (first
-     (for [x input
-           y input
+     (for [i (range input-count)
+           j (range i)
+           :let [x (input i)
+                 y (input j)]
            :when (= 2020 (+ x y))]
        (* x y)))))
 
+
 (defn part2
   []
-  (let [input (util/read-input "day01.txt" parse)]
+  (let [input (util/read-input "day01.txt" parse)
+        input-count (count input)]
     (first
-     (for [x input
-           y input
-           z input
+     (for [i (range input-count)
+           j (range i)
+           k (range j)
+           :let [x (input i)
+                 y (input j)
+                 z (input k)]
            :when (= 2020 (+ x y z))]
        (* x y z)))))
