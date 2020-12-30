eb399eb61c5bc9fc09dc8d1e9fd23ec2423058bf
Cayenne Geis <cayenne@hey.com>
2020-12-29 19:38:22 -0500

    day 18 part 2

--- y2020/src/y2020/day18.clj
+++ y2020/src/y2020/day18.clj
@@ -3,7 +3,7 @@
             [clojure.edn :as edn]
             [y2020.util :as util]))
 
-(defn compute-step
+(defn basic-compute-step
   [expr]
   (-> expr
       (string/replace #"(\(|^)(\d+) ([\+\*]) (\d+)"
@@ -13,20 +13,54 @@
                                     (edn/read-string n2)))))
       (string/replace #"\((\d+)\)" "$1")))
 
-(defn compute
-  [expr]
+(defn basic-compute [expr]
   (if (re-matches #"\d+" expr)
     (edn/read-string expr)
-    (recur (compute-step expr))))
+    (recur (basic-compute-step expr))))
 
 (defn part1*
   [input]
-  (apply + (map compute input)))
+  (apply + (map basic-compute input)))
 
 (def part1 (util/make-run-fn "day18" string/split-lines part1*))
 
-#_(def part2 (util/make-run-fn "day18" string/split-lines part2*))
+(defn operate-on-found-numbers
+  [expr regex op]
+  (string/replace expr
+                  regex
+                  (fn [region]
+                    (let [numbers (re-seq #"\d+" region)]
+                      (->> numbers
+                           (map edn/read-string)
+                           (apply op)
+                           (str))))))
+
+(defn add-available
+  [expr]
+  (-> expr
+      (operate-on-found-numbers #"\d+(?: \+ \d+)+" +)
+      (string/replace #"\((\d+)\)" "$1")))
+
+(defn multiply-available
+  [expr]
+  (operate-on-found-numbers expr #"\(\d+(?: \* \d+)+\)" *))
+
+(defn advanced-compute-step
+  [expr]
+  (-> expr add-available multiply-available))
+
+(defn advanced-compute [expr]
+  (cond
+    (re-matches #"\d+" expr) (edn/read-string expr)
+    (re-matches #"\d+.+" expr) (advanced-compute (str "(" expr ")"))
+    :else (recur (advanced-compute-step expr))))
+
+(defn part2*
+  [input]
+  (apply + (map advanced-compute input)))
+
+(def part2 (util/make-run-fn "day18" string/split-lines part2*))
 
 (comment "run"
-(part1)
-(part2))
+  (part1)
+  (part2))
--- y2020/test/y2020/day18_test.clj
+++ y2020/test/y2020/day18_test.clj
@@ -1,18 +1,56 @@
 (ns y2020.day18-test
   (:require [y2020.day18 :as d]
             [y2020.util :as util]
-            [clojure.test :refer [deftest is]]))
+            [clojure.test :refer [deftest testing is]]))
 
 (deftest compute-step-works
   (is (= "2" (d/compute-step "1 + 1")))
   (is (= "6 * 4 * 30" (d/compute-step "2 * 3 * 4 * (5 * 6)"))))
 
-(deftest compute-works
-  (is (= 51 (d/compute "1 + (2 * 3) + (4 * (5 + 6))")))
-  (is (= 26 (d/compute "2 * 3 + (4 * 5)")))
-  (is (= 437 (d/compute "5 + (8 * 3 + 9 + 3 * 4 * 3)")))
-  (is (= 12240 (d/compute "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")))
-  (is (= 13632 (d/compute "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"))))
+(deftest basic-compute-works
+  (is (= 51 (d/basic-compute "1 + (2 * 3) + (4 * (5 + 6))")))
+  (is (= 26 (d/basic-compute "2 * 3 + (4 * 5)")))
+  (is (= 437 (d/basic-compute "5 + (8 * 3 + 9 + 3 * 4 * 3)")))
+  (is (= 12240 (d/basic-compute "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")))
+  (is (= 13632 (d/basic-compute "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"))))
 
 (deftest part1
-  (is (= 1451467526514 (part1))))
+  (is (= 1451467526514 (d/part1))))
+
+(deftest parts-work-to-solve-example
+  (testing "first example"
+    (is (= "(1 + (2 * 3) + (4 * 11))"
+           (d/add-available "(1 + (2 * 3) + (4 * (5 + 6)))")))
+    (is (= "(1 + 6 + 44)"
+           (d/multiply-available "(1 + (2 * 3) + (4 * 11))")))
+    (is (= "51" (d/add-available "(1 + 6 + 44)"))))
+  (testing "third example"
+    (is (= "5 + (8 * 15 * 4 * 3)"
+           (d/add-available "5 + (8 * 3 + 9 + 3 * 4 * 3)")))
+    (is (= "5 + 1440"
+           (d/multiply-available "5 + (8 * 15 * 4 * 3)")))
+    (is (= "1445"
+           (-> "5 + 1440" d/add-available)))))
+
+(deftest advanced-compute-step-works
+  (testing "first example"
+    (is (= "1 + 6 + 44"
+           (d/advanced-compute-step "1 + (2 * 3) + (4 * (5 + 6))")))
+    (is (= "51"
+           (d/advanced-compute-step "(1 + 6 + 44)")))))
+
+(deftest advanced-compute-works
+  (is (= 51 (d/advanced-compute "1 + (2 * 3) + (4 * (5 + 6))")))
+  (is (= 46 (d/advanced-compute "2 * 3 + (4 * 5)")))
+  (is (= 1445 (d/advanced-compute "5 + (8 * 3 + 9 + 3 * 4 * 3)")))
+  (is (= 669060
+         (d/advanced-compute "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")))
+  (is
+   (= 23340
+      (d/advanced-compute "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"))))
+
+(deftest part2
+  (is (= 224973686321527 (d/part2))))
+
+(comment "run tests"
+  (clojure.test/run-tests))
