7822c8049d447e212beb5c645a1115661cea7c7c
Cayenne Geis <cayennegeis@icloud.com>
2022-12-20 20:01:36 -0500

    day 3 part 2

--- y2022/src/y2022/day03.clj
+++ y2022/src/y2022/day03.clj
@@ -32,7 +32,7 @@
 ;; * The fifth rucksack's compartments only share item type **t**.
 ;; * The sixth rucksack's compartments only share item type **s**.
 ;; 
-;; To help prioritize item rearrangement, every item type can be converted to a priority:
+;; To help prioritize item rearrangement, every item type can be converted to a **priority**:
 ;; 
 ;; * Lowercase item types a through z have priorities 1 through 26.
 ;; * Uppercase item types A through Z have priorities 27 through 52.
@@ -73,8 +73,8 @@ CrZsJsPPZsGzwwsLwLmpwMDw")
   (map split-compartments parsed-input))
 
 (defn common-item
-  [[compartment1 compartment2]]
-  (first (set/intersection (set compartment1) (set compartment2))))
+  [ss]
+  (first (apply set/intersection (map set ss))))
 
 (def example-common-items
   (map common-item example-split-contents))
@@ -97,3 +97,49 @@ CrZsJsPPZsGzwwsLwLmpwMDw")
 
 (def total
   (apply + priorities))
+
+;; ## Part Two
+;; 
+;; As you finish identifying the misplaced items, the Elves come to you with another issue.
+;; 
+;; For safety, the Elves are divided into groups of three. Every Elf carries a badge that identifies their group. For efficiency, within each group of three Elves, the badge is the **only item type carried by all three Elves**. That is, if a group's badge is item type `B`, then all three Elves will have item type `B` somewhere in their rucksack, and at most two of the Elves will be carrying any other item type.
+;; 
+;; The problem is that someone forgot to put this year's updated authenticity sticker on the badges. All of the badges need to be pulled out of the rucksacks so the new authenticity stickers can be attached.
+;; 
+;; Additionally, nobody wrote down which item type corresponds to each group's badges. The only way to tell which item type is the right one is by finding the one item type that is **common between all three Elves** in each group.
+;; 
+;; Every set of three lines in your list corresponds to a single group, but each group can have a different badge item type. So, in the above example, the first group's rucksacks are the first three lines:
+;; 
+;;     vJrwpWtwJgWrhcsFMMfFFhFp
+;;     jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+;;     PmmdzqPrVvPwwTWBwg
+;; 
+;; And the second group's rucksacks are the next three lines:
+;; 
+;;     wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+;;     ttgJtRGJQctTZtZT
+;;     CrZsJsPPZsGzwwsLwLmpwMDw
+;; 
+;; In the first group, the only item type that appears in all three rucksacks is lowercase `r`; this must be their badges. In the second group, their badge item type must be `Z`.
+;; 
+;; Priorities for these items must still be found to organize the sticker attachment efforts: here, they are 18 (`r`) for the first group and 52 (`Z`) for the second group. The sum of these is **70**.
+;; 
+;; Find the item type that corresponds to the badges of each three-Elf group. **What is the sum of the priorities of those item types?**
+
+(def example-groups
+  (partition 3 parsed-example))
+
+(def groups
+  (partition 3 parsed-input))
+
+(def example-badge-items
+  (map common-item example-groups))
+
+(def badge-items
+  (map common-item groups))
+
+(def example-total-priorities-2
+  (apply + (map priority example-badge-items)))
+
+(def total-priorities-2
+  (apply + (map priority badge-items)))
